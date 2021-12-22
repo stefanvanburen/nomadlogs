f30f217c928d44d51c6bd1a38cc27df1af609a6d
Stefan VanBuren <stefan@vanburen.xyz>
2021-12-22 11:18:43 -0500

    refactor: Use the nomad sdk to access job logs
    Much cleaner than the previous version. Still a few issues though - if
    Nomad is shut down, a panic occurs accessing frame.Data, so we should
    probably take a look at that.

--- go.mod
+++ go.mod
@@ -3,6 +3,16 @@ module github.com/blues/hub/tools/vagrantLogs
 go 1.17
 
 require (
+	github.com/hashicorp/nomad/api v0.0.0-20211220150801-0ec5db432fe4
 	github.com/oklog/run v1.1.0
 	github.com/peterbourgon/ff/v3 v3.1.0
+)
+
+require (
+	github.com/gorilla/websocket v1.4.1 // indirect
+	github.com/hashicorp/cronexpr v1.1.1 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/mitchellh/mapstructure v1.4.2 // indirect
 )
--- main.go
+++ main.go
@@ -1,129 +1,149 @@
 package main
 
 import (
-	"bufio"
 	"context"
 	"flag"
 	"fmt"
 	"os"
-	"os/exec"
 	"strings"
+	"time"
 
+	nomad "github.com/hashicorp/nomad/api"
 	okrun "github.com/oklog/run"
 	"github.com/peterbourgon/ff/v3"
 )
 
 func main() {
 	if err := run(os.Args[1:]); err != nil {
-		fmt.Printf("vagrant-logs err: %s", err)
+		fmt.Printf("nomadlogs err: %s", err)
 		os.Exit(1)
 	}
 }
 
-// Still TODO:
-// 1. when the watchers go down, try to start up again at some point in the future?
-//    Maybe a ticker that wraps the jobs?
-// 2. confirm that when we ctrl-c out, we successfully shut down the goroutines
-
 func run(args []string) error {
-	fs := flag.NewFlagSet("vagrant-logs", flag.ExitOnError)
+	fs := flag.NewFlagSet("nomadlogs", flag.ExitOnError)
 	var (
-		jobs = fs.String("services", "noteboard,noteadmin,notesim-api,public_notehandler,public_notediscovery", "comma-separated list of jobs to watch")
+		// noteadmin,notesim-api
+		// job                  -> task
+		// public_notediscovery -> notediscovery
+		// noteboard            -> noteboard
+		// public_notehandler   -> public_notehandler
+		// ...
+		jobs = fs.String("jobs", "noteboard,public_notehandler", "comma-separated list of jobs to watch")
 	)
 
-	if err := ff.Parse(fs, args, ff.WithEnvVarPrefix("VAGRANT_LOGS")); err != nil {
+	if err := ff.Parse(fs, args, ff.WithEnvVarPrefix("NOMAD_LOGS")); err != nil {
 		return fmt.Errorf("could not parse flags: %s", err)
 	}
 
-	jobsToWatch := strings.Split(*jobs, ",")
-
-	logs := make(chan msg, 100)
+	client, err := nomad.NewClient(nomad.DefaultConfig())
+	if err != nil {
+		return fmt.Errorf("could not create nomad client: %s", err)
+	}
 
 	ctx := context.Background()
+
 	var g okrun.Group
-
-	for _, job := range jobsToWatch {
+	for _, job := range strings.Split(*jobs, ",") {
 		job := job
-		// watch stdout
 		g.Add(func() error {
-			return watchJob(ctx, job, logs, false)
-		}, func(err error) {
-			// TODO: anything useful to do here?
-		})
+			jw := jobWatcher{
+				name:   job,
+				client: client,
+			}
 
-		// watch stderr
-		g.Add(func() error {
-			return watchJob(ctx, job, logs, true)
+			return jw.run(ctx)
 		}, func(err error) {
-			// TODO: anything useful to do here?
+
 		})
 	}
-
-	g.Add(func() error {
-		return printLogs(ctx, logs)
-	}, func(err error) {
-		// TODO: anything useful to do here?
-	})
 
 	if err := g.Run(); err != nil {
-		return fmt.Errorf("error watching job: %s", err)
+		return fmt.Errorf("got error: %s", err)
 	}
 
 	return nil
 }
 
-type msg struct {
-	service string
-	log     string
+type jobWatcher struct {
+	name   string
+	client *nomad.Client
 }
 
-func watchJob(ctx context.Context, job string, logs chan<- msg, isStdErr bool) error {
-	commandName := "vagrant"
-	args := []string{"ssh", "client", "--", "nomad", "alloc", "logs", "-tail", "-f", "-job", job}
-	if isStdErr {
-		args = []string{"ssh", "client", "--", "nomad", "alloc", "logs", "-tail", "-f", "-stderr", "-job", job}
-	}
+func (jw *jobWatcher) run(ctx context.Context) error {
+	fmt.Printf("watching job %s\n", jw.name)
 
-	cmd := exec.CommandContext(ctx, commandName, args...)
-
-	cmdOut, err := cmd.StdoutPipe()
-	if err != nil {
-		return fmt.Errorf("could not get pipe to stdout: %s", err)
-	}
-
-	fmt.Printf("starting command %s...\n", cmd)
-
-	if err := cmd.Start(); err != nil {
-		return fmt.Errorf("could not start command: %s", err)
-	}
-
-	scanner := bufio.NewScanner(cmdOut)
-
-	for scanner.Scan() {
-		logs <- msg{
-			service: job,
-			log:     scanner.Text(),
+	for {
+		allocationList, _, err := jw.client.Allocations().List(nil)
+		if err != nil {
+			fmt.Printf("meta: could not list nomad allocations. nomad probably isn't running? waiting 30 seconds before trying again: %s\n", err)
+			time.Sleep(30 * time.Second)
+			continue
 		}
-	}
 
-	if err := scanner.Err(); err != nil {
-		return fmt.Errorf("scanner error: %s", err)
-	}
+		var allocationID string
+		for _, allocationStub := range allocationList {
+			if allocationStub.JobID == jw.name && allocationStub.ClientStatus == "running" {
+				allocationID = allocationStub.ID
+				break
+			}
+		}
 
-	if err := cmd.Wait(); err != nil {
-		return fmt.Errorf("error waiting for command: %s", err)
-	}
+		if allocationID == "" {
+			fmt.Printf("meta: no allocations running for %s; waiting for 30 seconds before trying again\n", jw.name)
+			time.Sleep(30 * time.Second)
+			continue
+		}
 
-	return nil
+		allocation, _, err := jw.client.Allocations().Info(allocationID, nil)
+		if err != nil {
+			return fmt.Errorf("could not retrieve allocation: %s", err)
+		}
+
+		// watch the stream until it's done
+		jw.watchStream(ctx, allocation)
+	}
 }
 
-func printLogs(ctx context.Context, logs <-chan msg) error {
+func (jw *jobWatcher) watchStream(ctx context.Context, allocation *nomad.Allocation) error {
+	stdoutFrames, stdoutErrChan := jw.client.AllocFS().Logs(allocation, true, jw.name, "stdout", "end", 0, nil, nil)
+	stderrFrames, stderrErrChan := jw.client.AllocFS().Logs(allocation, true, jw.name, "stderr", "end", 0, nil, nil)
+
 	for {
 		select {
-		case log := <-logs:
-			fmt.Printf("%s: %s\n", log.service, log.log)
-		case <-ctx.Done():
-			return ctx.Err()
+		case stdoutFrame := <-stdoutFrames:
+			// We're probably getting multiple lines.
+			raw := string(stdoutFrame.Data)
+			ss := strings.Split(raw, "\n")
+
+			for _, s := range ss {
+				if s == "" {
+					continue
+				}
+
+				fmt.Printf("%s: %s\n", jw.name, s)
+			}
+
+		case stderrFrame := <-stderrFrames:
+			// We're probably getting multiple lines.
+			raw := string(stderrFrame.Data)
+			ss := strings.Split(raw, "\n")
+
+			for _, s := range ss {
+				if s == "" {
+					continue
+				}
+
+				fmt.Printf("%s: %s\n", jw.name, s)
+			}
+
+		case err := <-stdoutErrChan:
+			fmt.Printf("meta: %s: got error (allocation probably shutting down): %s\n", jw.name, err)
+			return nil
+		case err := <-stderrErrChan:
+			fmt.Printf("meta: %s: got error (allocation probably shutting down): %s\n", jw.name, err)
+			return nil
 		}
 	}
+
 }
